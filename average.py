21d6dba89a0fecc4268eef67643c5ca0bb1d6989
Capsen Lu <capsenlu@Capsens-MacBook-Pro.local>
2022-10-27 13:23:29 +1000

    add number check

--- average.py
+++ average.py
@@ -1,10 +1,13 @@
 number_count=input("Please enter how many number you going to enter: ")
-i=0
-number_list=[]
-while i<int(number_count):
-    number_list.append(input("please enter a number: "))
-    i += 1
-sum=0
-for number in number_list:
-    sum += float(number)
-print("The average number is " + str(sum/len(number_list)))
+if number_count.isdecimal():
+    i=0
+    number_list=[]
+    while i<int(number_count):
+        number_list.append(input("please enter a number: "))
+        i += 1
+    sum=0
+    for number in number_list:
+        sum += float(number)
+    print("The average number is " + str(sum/len(number_list)))
+else :
+    print("The text you entered is not a number!")
